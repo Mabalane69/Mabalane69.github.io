9e2133d868e7d7329d3e1febde6216ebf6ef20a1
Mabalane Aphane <maphane023a@student.wethinkcode.co.za>
2024-04-17 14:18:46 +0200

    updated the navigations for the projects page

--- navigation.js
+++ navigation.js
@@ -10,9 +10,15 @@ function showAbout() {
 
 function showContact() {
     document.getElementById('homeSection').style.display = 'none';
-    document.getElementById('aboutSection').style.display = 'block';
+    document.getElementById('aboutSection').style.display = 'none';
+}
+
+function showProjects() {
+    document.getElementById('homeSection').style.display = 'none';
+    document.getElementById('aboutSection').style.display = 'none';
 }
 
 document.getElementById('homeLink').addEventListener('click', showHome);
 document.getElementById('aboutLink').addEventListener('click', showAbout);
 document.getElementById('contactLink').addEventListener('click', showContact);
+document.getElementById('projectsLink').addEventListener('click', showProjects);
